966dad3368c1c91b1ad465ade0ffd552efcbaf97
SaidAsaad <saidyousef201513@gmail.com>
2025-06-11 17:23:11 -0300

    initial commit

---
+++ src/test/java/com/said/simple_crud/SimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.said.simple_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
