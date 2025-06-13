b26845a4c8df45aecdd632bd667ed351abe6148b
SaidAsaad <saidyousef201513@gmail.com>
2025-06-12 22:04:54 -0300

    Get methods implementation

--- src/main/java/com/said/simple_crud/services/ProductService.java
+++ src/main/java/com/said/simple_crud/services/ProductService.java
@@ -1,8 +1,12 @@
 package com.said.simple_crud.services;
 
+import com.said.simple_crud.infrastructure.entities.Category;
 import com.said.simple_crud.infrastructure.entities.Product;
 import com.said.simple_crud.infrastructure.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -15,5 +19,14 @@ public class ProductService {
 
     public Product insert(Product entity){
         return repository.saveAndFlush(entity);
+    }
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> entity = repository.findById(id);
+        return entity.get();
     }
 }
